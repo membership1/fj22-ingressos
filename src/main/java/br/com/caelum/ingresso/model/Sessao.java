8720eb11d966fd697028aebe85fb3c251cd2adaa
membership1 <kleber.andrade@gmail.com>
2017-11-18 15:29:48 -0200

    novo

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,61 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+
+@Entity
+public class Sessao {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private LocalTime horario;
+
+	@ManyToOne
+	private Filme filme;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	@ManyToOne
+	private Sala sala;
+
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao()	{
+	}
+	public Sessao (LocalTime horario, Filme filme, Sala sala){
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+
+	}
+}
