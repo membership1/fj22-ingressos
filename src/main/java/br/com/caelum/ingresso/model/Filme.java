999bc0a2b7fc4ed4ba10523bc7a4f674d3cc9079
membership1 <kleber.andrade@gmail.com>
2017-11-25 11:36:28 -0200

    all

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -13,7 +13,7 @@ import java.time.Duration;
 @Entity
 public class Filme {
 
-    public BigDecimal getPreco() {
+	public BigDecimal getPreco() {
 		return preco;
 	}
 
@@ -21,68 +21,63 @@ public class Filme {
 		this.preco = preco;
 	}
 
-	public void setDuracao(Duration duracao) {
-		this.duracao = duracao;
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String nome;
+	private Duration duracao;
+	private String genero;
+	private BigDecimal preco;
+
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Filme() {
+
 	}
 
-	@Id
-    @GeneratedValue
-    private Integer id;
-    private String nome;
-    private Duration duracao;
-    private String genero;
-    private BigDecimal preco;
-    
-    
-    /**
-     * @deprecated hibernate only
-     */
-    public Filme() {
-
-    }
-    
-    public Filme(String nome, Duration duracao, String genero) {
+	public Filme(String nome, Duration duracao, String genero) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
 	}
 
 	public Integer getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public Duration getDuracao() {
-        return duracao;
-    }
+	public Duration getDuracao() {
+		return duracao;
+	}
 
-    public void setDuracao(long duracao) {
-        this.duracao = Duration.ofMinutes(duracao);
-    }
+	public void setDuracao(long duracao) {
+		this.duracao = Duration.ofMinutes(duracao);
+	}
 
-    public String getGenero() {
-        return genero;
-    }
+	public String getGenero() {
+		return genero;
+	}
 
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-    
-    public Filme(String nome, Duration duracao, String genero, BigDecimal preco){
-    	this.nome = nome;
-    	this.duracao = duracao;
-    	this.genero = genero;
-    	this.preco = preco;
-    }
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
+		this.nome = nome;
+		this.duracao = duracao;
+		this.genero = genero;
+		this.preco = preco;
+	}
 }
